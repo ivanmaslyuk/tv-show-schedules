fd79e4ec057bd75f985ae78730b7b30e708588c1
Ivan Maslyuk <ivan.maslyuck@live.com>
2026-06-22 16:24:20 +0400

    fix cape fear episode titles

--- migrations/V3__show_watches_and_cape_fear.sql
+++ migrations/V3__show_watches_and_cape_fear.sql
@@ -13,16 +13,16 @@ INSERT INTO seasons (id, number, show_id, release_date) VALUES
 (120101, 1, 120001, '2026-06-05');
 
 INSERT INTO episodes (id, title, number, season_id, release_date) VALUES
-(120201, 'Episode 1', 1, 120101, '2026-06-05'),
-(120202, 'Episode 2', 2, 120101, '2026-06-05'),
-(120203, 'Episode 3', 3, 120101, '2026-06-12'),
-(120204, 'Episode 4', 4, 120101, '2026-06-19'),
-(120205, 'Episode 5', 5, 120101, '2026-06-26'),
-(120206, 'Episode 6', 6, 120101, '2026-07-03'),
-(120207, 'Episode 7', 7, 120101, '2026-07-10'),
-(120208, 'Episode 8', 8, 120101, '2026-07-17'),
-(120209, 'Episode 9', 9, 120101, '2026-07-24'),
-(120210, 'Episode 10', 10, 120101, '2026-07-31');
+(120201, 'Fingers & Toes', 1, 120101, '2026-06-05'),
+(120202, 'Why Would I Want to Hurt You?', 2, 120101, '2026-06-05'),
+(120203, 'Phantom Sensations', 3, 120101, '2026-06-12'),
+(120204, 'Pierced', 4, 120101, '2026-06-19'),
+(120205, 'Faith', 5, 120101, '2026-06-26'),
+(120206, 'Possum', 6, 120101, '2026-07-03'),
+(120207, 'Mongrel', 7, 120101, '2026-07-10'),
+(120208, 'Los tiempos de Dios son Perfectos', 8, 120101, '2026-07-17'),
+(120209, 'The Scar', 9, 120101, '2026-07-24'),
+(120210, 'The Executioners', 10, 120101, '2026-07-31');
 
 SELECT setval(pg_get_serial_sequence('shows', 'id'), (SELECT MAX(id) FROM shows));
 SELECT setval(pg_get_serial_sequence('seasons', 'id'), (SELECT MAX(id) FROM seasons));
